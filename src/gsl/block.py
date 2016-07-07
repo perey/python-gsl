4f0c8abf9f238f8b75e3e5a614a1e6389a289224
Tim Pederick <pederick@gmail.com>
2016-07-07 23:04:44 +0800

    And this should've been in __all__.

--- src/gsl/block.py
+++ src/gsl/block.py
@@ -23,7 +23,7 @@
 # You should have received a copy of the GNU General Public License
 # along with python-gsl. If not, see <http://www.gnu.org/licenses/>.
 
-__all__ = ['alloc', 'calloc', 'free']
+__all__ = ['gsl_block_p', 'alloc', 'calloc', 'free']
 
 # Standard library imports.
 from ctypes import Structure, c_double, c_size_t, POINTER
