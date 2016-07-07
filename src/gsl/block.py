13d04227af0302a96c82ab0885945a2da9c336bc
Tim Pederick <pederick@gmail.com>
2016-07-07 23:02:50 +0800

    Forgot to define __all__.

--- src/gsl/block.py
+++ src/gsl/block.py
@@ -23,7 +23,7 @@
 # You should have received a copy of the GNU General Public License
 # along with python-gsl. If not, see <http://www.gnu.org/licenses/>.
 
-__all__ = []
+__all__ = ['alloc', 'calloc', 'free']
 
 # Standard library imports.
 from ctypes import Structure, c_double, c_size_t, POINTER
